b5fbbf5db7a3b5272d313b6a622c6bfc6322abc7
S <shaunkurian345@gmail.com>
2024-04-23 00:32:48 -0500

    Added german end game screens.

--- ECE319K_Lab9H/GameDoneMenu.cpp
+++ ECE319K_Lab9H/GameDoneMenu.cpp
@@ -11,17 +11,17 @@ bool lastIn = 0xFFFF;
 
 void printWinLose(void)
 {
-    bool ready = false;
+    ready = false;
     ST7735_FillScreen(ST7735_BLACK);
     if(gameWinner() == !host)
-        ST7735_DrawBitmap(15, 75, YouWin, 130, 30);
+        ST7735_DrawBitmap(15, 75, currLang == ENGLISH ? YouWin : YouWinGerman, 130, 30);
     else
-        ST7735_DrawBitmap(10, 80, YouLose, 139, 42);
+        ST7735_DrawBitmap(10, 80, currLang == ENGLISH ? YouLose : YouLoseGerman, 139, 42);
 }
 
 void updateScreen(int input)
 {
-    if(!lastIn && (input & 0x000F))
+    if(!lastIn && (input & 0x00FF) != 0)
         ready = true;
     
     lastIn = input;
